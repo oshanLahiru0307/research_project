8dd1281fb63f4b2f311c3210083d311688481dea
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-12-31 15:47:43 +0530

    dashboard is created

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import Header from './Header'
+import Sidebar from './Sidebar'
+
+function Layout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = useState(true)
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div className="flex">
+        <Sidebar sidebarOpen={sidebarOpen} />
+        <main className={`flex-1 transition-all duration-300 ${sidebarOpen ? 'lg:ml-64' : 'ml-0'} mt-16`}>
+          <div className="p-6">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
+
