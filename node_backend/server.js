6a7a46358d440281d2e0986e6a02b4395824b665
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-02-24 14:38:45 +0530

    server.js file is updated

--- node_backend/server.js
+++ node_backend/server.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
+const userRoutes = require('./Routes/UserRoute');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+//user routes
+app.use('/api', userRoutes);
+
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
