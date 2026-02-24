a0759920cb2be505a310bc46f5ca1246f1109e08
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-02-24 09:25:45 +0530

    server.js file is updated

--- node_backend/server.js
+++ node_backend/server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
+    console.log('Connected to MongoDB');
     app.listen(process.env.PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
     });
