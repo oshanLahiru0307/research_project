f571ea8a74d3a84ac71dbc65c025d2529bbf77c3
oshanLahiru0307 <klahiruoshan@gmail.com>
2026-02-24 14:31:22 +0530

    UserRoutes are completed

---
+++ node_backend/Routes/UserRoute.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const UserController{
+    createUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser
+} = require('../Controllers/UserController');
+
+// Create a new user
+router.post('/users', createUser);
+
+// Get all users
+router.get('/users', getAllUsers);
+
+// Get a user by ID
+router.get('/users/:id', getUserById);
+
+// Update a user by ID
+router.put('/users/:id', updateUser);
+
+// Delete a user by ID
+router.delete('/users/:id', deleteUser);
+
+module.exports = router;
